04d1645124bd8a64c052c0e21d7c91c92bed89e8
Grixtynine <grixyirlaz7@gmail.com>
2026-03-10 14:23:20 -0500

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css';
 import ProjectCard from "./components/ProjectCard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+// Example project pages
+function Calculator() {
+  return <h2>Calculator Page</h2>;
+}
+function TodoApp() {
+  return <h2>Todo App Page</h2>;
+}
+function WeatherApp() {
+  return <h2>Weather App Page</h2>;
+}
+
+function Home() {
   return (
     <>
       <div className="overlay"></div>
@@ -9,12 +21,25 @@ function App() {
       <div className="content">
         <h1>My Projects</h1>
         <div className="projects">
-          <ProjectCard title="Calculator" />
-          <ProjectCard title="Todo App" />
-          <ProjectCard title="Weather App" />
+          <ProjectCard title="Calculator" link="/calculator" />
+          <ProjectCard title="Todo App" link="/todo" />
+          <ProjectCard title="Weather App" link="/weather" />
         </div>
       </div>
     </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/todo" element={<TodoApp />} />
+        <Route path="/weather" element={<WeatherApp />} />
+      </Routes>
+    </Router>
   );
 }
 
