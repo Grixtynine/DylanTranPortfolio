bd77b5b16d01696e0db1dbb549212653b1dc6579
Grixtynine <grixyirlaz7@gmail.com>
2026-04-08 16:11:45 -0500

    Update Dock.jsx

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -6,16 +6,17 @@ import { Link } from "react-router-dom";
 import homeImg from "../assets/globe.png";
 import contactImg from "../assets/contact.png";
 import GameAssetImg from "../assets/gameasset.png";
-import GraphicDesignImg from '../assets/";
+import GraphicDesignImg from "../assets/";
+import SchoolProjImg from "../assets/";
   
 export default function Dock() {
   const [mouseY, setMouseY] = useState(null);
 
   const items = [
     { id: 1, icon: homeImg, path: "/" , tag: "Home"},
-    { id: 1, icon: GraphicDesignImg, path: "/GraphicDesign", tag: "Graphic design" },
-    { id: 1, icon: SchoolProgImg, path: "/SchoolProjects", tag: "School Projects" },
-    { id: 3, icon: GameAssetImg, path: "/GameDev" , tag: "Game Assets"},
+    { id: 1, icon: GraphicDesignImg, path: "/graphicdesign", tag: "Graphic design" },
+    { id: 1, icon: SchoolProjImg, path: "/schoolprojects", tag: "School Projects" },
+    { id: 3, icon: GameAssetImg, path: "/gamedev" , tag: "Game Assets"},
     { id: 5, icon: contactImg, path: "/contact" , tag: "Contact"},
   ];
 
